73e4bf34bb6d0c612e01e7b2fa98f57a8870e58b
shrutipatel9 <shrutikamani024@gmail.com>
2024-01-30 14:21:19 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '700911d1-ffe2-4247-b69e-34f4c6ea424b';
+export const MODAL_CLOSE_SEC = 2.5;
